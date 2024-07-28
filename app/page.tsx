7e58177b0a6fba81fa9b760907fc2858a6896a8a
VinhNguyen0301 <nguyenvinh.fit@gmail.com>
2024-07-28 14:31:00 +0700

    Add new commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Calculate } from "@/components/calculate";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Calculate />
+    </main>
+  );
+}
